31c21ddf069147b7231233b37a259275046eda88
Ahmettcakar <116648416+Ahmettcakar@users.noreply.github.com>
2025-02-11 15:25:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 import json
 
 def check_for_updates():
-    print("test son")
+    print("test")
     """Updater'ı çalıştırarak eksik dosyaları kontrol eder."""
     result = subprocess.run(["python", "updater.py"], capture_output=True, text=True)
     
