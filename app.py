de8cfec13452d0070a62645dcf4e225538f993ef
Ahmettcakar <116648416+Ahmettcakar@users.noreply.github.com>
2025-02-11 14:48:15 +0300

    asdasd app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sys
 import requests
 
 def check_for_updates():
-    print("alksdjflaksjdla")
+    print("asdasd")
     """Updater'ı çalıştırarak eksik dosyaları kontrol eder."""
     result = subprocess.run(["python", "updater.py"], capture_output=True, text=True)
     
